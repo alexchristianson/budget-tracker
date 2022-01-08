d3f50281a7965621d3d222521e7a27900fbb3ae9
Alex C <alex.christianson.05@gmail.com>
2022-01-08 14:06:07 -0600

    added additional icons to be cached

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,15 @@ const cacheURL = [
     "./js/index.js",
     "./manifest.json",
     "./css/styles.css",
-    "./index.html"
+    "./index.html",
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png'
 ]
 
 self.addEventListener('install', function(evt) {
